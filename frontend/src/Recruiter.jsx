396b1ea57e5594fa00ec846061d6b16a4da94561
Arkaaz <arkaazattar@gmail.com>
2026-02-04 15:55:13 -0500

    Fixed jsx issues and prepare for recruiter page
    - recruiter flask route had some db issues that should be fixed now
    - recruiter jsx side is now ready to implement fetching with flask route
    - next thing to do is to create the flask route for the frontend

--- frontend/src/Recruiter.jsx
+++ frontend/src/Recruiter.jsx
@@ -3,74 +3,84 @@ import { useNavigate } from "react-router";
 
 function Recruiter() {
   const navigate = useNavigate();
-  const [requiredLeague, setRequiredLeague] = useState(0);
+  const [requiredLeague, setRequiredLeague] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // this needs to go to the api route for recuriter.
+    console.log(requiredLeague)
+  }
+    useEffect (() => {
+      fetch("/recruiter", {
+        // tbw
+      })
+    })
 
   return (
     
     <div>
     <label htmlFor="leagues">Enter Required League</label>
-    <form>
+    <form onSubmit={handleSubmit}>
 
       <select
         id="leagues"
         name="required_league"
         value={requiredLeague}
-        onChange={(e) => setRequiredLeague(e.target.value)}
+        onChange={(e) => setRequiredLeague(Number(e.target.value))}
         required
       >
-        <option value="" disabled>
+        <option value={""} disabled>
           -- Select an option --
         </option>
 
-        <option value="1">Skeleton 1</option>
-        <option value="2">Skeleton 2</option>
-        <option value="3">Skeleton 3</option>
+        <option value={0}>Unranked</option>
 
-        <option value="4">Barbarian 4</option>
-        <option value="5">Barbarian 5</option>
-        <option value="6">Barbarian 6</option>
+        <option value={1}>Skeleton 1</option>
+        <option value={2}>Skeleton 2</option>
+        <option value={3}>Skeleton 3</option>
 
-        <option value="7">Archer 7</option>
-        <option value="8">Archer 8</option>
-        <option value="9">Archer 9</option>
+        <option value={4}>Barbarian 4</option>
+        <option value={5}>Barbarian 5</option>
+        <option value={6}>Barbarian 6</option>
 
-        <option value="10">Wizard 10</option>
-        <option value="11">Wizard 11</option>
-        <option value="12">Wizard 12</option>
+        <option value={7}>Archer 7</option>
+        <option value={8}>Archer 8</option>
+        <option value={9}>Archer 9</option>
 
-        <option value="13">Valkyrie 13</option>
-        <option value="14">Valkyrie 14</option>
-        <option value="15">Valkyrie 15</option>
+        <option value={10}>Wizard 10</option>
+        <option value={11}>Wizard 11</option>
+        <option value={12}>Wizard 12</option>
 
-        <option value="16">Witch 16</option>
-        <option value="17">Witch 17</option>
-        <option value="18">Witch 18</option>
+        <option value={13}>Valkyrie 13</option>
+        <option value={14}>Valkyrie 14</option>
+        <option value={15}>Valkyrie 15</option>
 
-        <option value="19">Golem 19</option>
-        <option value="20">Golem 20</option>
-        <option value="21">Golem 21</option>
+        <option value={16}>Witch 16</option>
+        <option value={17}>Witch 17</option>
+        <option value={18}>Witch 18</option>
 
-        <option value="22">P.E.K.K.A 22</option>
-        <option value="23">P.E.K.K.A 23</option>
-        <option value="24">P.E.K.K.A 24</option>
+        <option value={19}>Golem 19</option>
+        <option value={20}>Golem 20</option>
+        <option value={21}>Golem 21</option>
 
-        <option value="25">Electro Titan 25</option>
-        <option value="26">Electro Titan 26</option>
-        <option value="27">Electro Titan 27</option>
+        <option value={22}>P.E.K.K.A 22</option>
+        <option value={23}>P.E.K.K.A 23</option>
+        <option value={24}>P.E.K.K.A 24</option>
 
-        <option value="28">Dragon 28</option>
-        <option value="29">Dragon 29</option>
-        <option value="30">Dragon 30</option>
+        <option value={25}>Electro Titan 25</option>
+        <option value={26}>Electro Titan 26</option>
+        <option value={27}>Electro Titan 27</option>
 
-        <option value="31">Electro Dragon 31</option>
-        <option value="32">Electro Dragon 32</option>
-        <option value="33">Electro Dragon 33</option>
+        <option value={28}>Dragon 28</option>
+        <option value={29}>Dragon 29</option>
+        <option value={30}>Dragon 30</option>
 
-        <option value="34">Legend League</option>
+        <option value={31}>Electro Dragon 31</option>
+        <option value={32}>Electro Dragon 32</option>
+        <option value={33}>Electro Dragon 33</option>
+
+        <option value={34}>Legend League</option>
       </select>
-        <button onSubmit={(e) =>
-          e.preventDefault()
-        }>Submit</button>
+        <button>Submit</button>
         </form>
   </div>
 
--- routes/recruiter_route.py
+++ routes/recruiter_route.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, session, render_template
 from ..api.recruiter_api import Recruiter
 from ..config import headers
 from ..services.mongo_db_client import clan_collection
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 recruiter_bp = Blueprint("recruiter", __name__)
 
@@ -23,21 +23,22 @@ def recruit():
         "clan_tag": session.get("clan_tag"),
         "player_tag": session.get("player_tag"),
         "clan_info": clan_info,
-        "last_updated" : datetime.now()
+        "last_updated" : datetime.now(timezone.utc),
+        "expires": datetime.now(timezone.utc) + timedelta(days = 7)
     }
     render_data = data.copy()
     
     existing = clan_collection.find_one({"clan_tag": data["clan_tag"]})
     if not existing:
         clan_collection.insert_one(data)
-        clan_collection.create_index("last_updated", expireAfterSeconds=604800)
+        clan_collection.create_index("expires", expireAfterSeconds=0)
 
     else: 
         clan_collection.update_one({"clan_tag" : session.get("clan_tag")}, 
                                    {'$set' : 
                                     {"requirements" : requirements, 
                                      "clan_info" : clan_info,
-                                     "last_updated" : datetime.now()
+                                     "last_updated" : datetime.now(timezone.utc)
                                      }})
 
     return render_template("recruiter.html", data = render_data)
